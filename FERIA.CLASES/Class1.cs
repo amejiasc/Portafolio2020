c09cf4a137f82abcdaafa1a9ad3927d15625bcf8
almejiasc <a.mejiasc@alumnos.duoc.cl>
2020-09-09 21:25:50 -0300

    Prueba de Subida

---
+++ FERIA.CLASES/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FERIA.CLASES
+{
+    public class Class1
+    {
+    }
+}
