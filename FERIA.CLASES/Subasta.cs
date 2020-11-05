3863bd5db632c86b3ad7dc833d71dc9376bfb6e7
almejiasc <a.mejiasc@alumnos.duoc.cl>
2020-11-04 21:02:06 -0300

    Clase de Subasta

---
+++ FERIA.CLASES/Subasta.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FERIA.CLASES
+{
+    public class Subasta
+    {
+        public int IdSubasta { get; set; }
+        public DateTime FechaSubasta { get; set; }
+        public DateTime FechaTermino { get; set; }
+        public bool Estado { get; set; }
+        public int IdProceso { get; set; }
+        public List<DetalleSubasta> DetalleSubasta { get; set; }
+
+        public Subasta() 
+        { 
+
+        }
+    }
+    public class DetalleSubasta
+    {
+        public int IdDetalle { get; set; }
+        public int MontoOferta { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Estado { get; set; }
+        public int IdSubasta { get; set; }
+        public int IdUsuario { get; set; }
+        public DetalleSubasta() {
+            Estado = "ENPROCESO";
+        }
+    }
+
+    public class RespuestaSubastaListar
+    {
+        public bool Exito { get; set; }
+        public string Mensaje { get; set; }
+        public List<Subasta> Subastas { get; set; }
+        public RespuestaSubastaListar()
+        {
+            Exito = true;
+            Mensaje = string.Empty;
+            Subastas = new List<Subasta>();
+        }
+    }
+    public class RespuestaSubasta
+    {
+        public bool Exito { get; set; }
+        public string Mensaje { get; set; }
+        public Subasta Subasta { get; set; }
+        public RespuestaSubasta()
+        {
+            Exito = true;
+            Mensaje = string.Empty;
+            Subasta = new Subasta();
+        }
+    }
+
+}
